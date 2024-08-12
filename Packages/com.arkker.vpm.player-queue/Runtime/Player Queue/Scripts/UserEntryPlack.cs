53ddd7c90000c6937a3f8be28eb3f203238d3817
aRkker <arkkerx@gmail.com>
2024-08-12 14:41:00 +0300

    1.0.5 fixing asmdef issues

--- Packages/com.arkker.vpm.player-queue/Runtime/Player Queue/Scripts/UserEntryPlack.cs
+++ Packages/com.arkker.vpm.player-queue/Runtime/Player Queue/Scripts/UserEntryPlack.cs
@@ -1,5 +1,4 @@
 ﻿
-using System;
 using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
@@ -7,47 +6,8 @@ using VRC.Udon;
 
 public class UserEntryPlack : UdonSharpBehaviour
 {
-    [NonSerialized] public QueueSystem qs; // Reference to the queue system
     void Start()
     {
-        // We need to find the queue system in one of our parent objects. This is a bit of a hack, but it works.
-
-        Transform parent = transform.parent;
-        while (parent != null)
-        {
-            qs = parent.GetComponent<QueueSystem>();
-            if (qs != null)
-            {
-                break;
-            }
-            parent = parent.parent;
-        }
-
-        if (qs == null)
-        {
-            Debug.LogError("UserEntryPlack: Could not find QueueSystem in parent objects.");
-        }
-
-    }
-
-    public void MoveUp()
-    {
-        // we need to get our index and call it on the queue system
-        int index = transform.GetSiblingIndex();
-        qs.MovePlayerPlackUp(index);
-    }
-
-    public void MoveDown()
-    {
-        // we need to get our index and call it on the queue system
-        int index = transform.GetSiblingIndex();
-        qs.MovePlayerPlackDown(index);
-    }
-
-    public void Delete()
-    {
-        // we need to get our index and call it on the queue system
-        int index = transform.GetSiblingIndex();
-        qs.DeleteUserByIndex(index);
+        
     }
 }
